8ddd2121855c8f7115c946cd748683e0a09df213
SarthakJain29 <sarthakjain2907@gmail.com>
2024-08-22 00:20:07 +0530

    login

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/user.js');
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'efjqfjkf13rnjn';
 
 app.use(express.json());
 app.use(cors());
@@ -31,5 +33,34 @@ app.post('/register', async (req,res)=>{
     }
     
 })
+
+app.post('/login', async (req, res) => {
+    console.log('Login endpoint hit'); // This should print on every request to /login
+    const { email, password } = req.body;
+    console.log('Login attempt for email:', email);
+    const userDoc = await User.findOne({ email });
+
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign(
+                { email: userDoc.email, id: userDoc._id }, 
+                jwtSecret, 
+                {}, 
+                (err, token) => {
+                    if (err) throw err;
+                    console.log('Generated JWT:', token); 
+                    res.cookie('token', token, { httpOnly: true }).json({ token, message: 'pass ok' });
+                }
+            );
+        } else {
+            res.status(401).json({ message: 'Incorrect password' });
+        }
+    } else {
+        res.status(422).json({ message: 'User not found' });
+    }
+});
+
+
 
 app.listen(4000);
